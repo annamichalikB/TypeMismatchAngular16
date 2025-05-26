8dea8e825e9ab924398bac5be15d9544a14996c8
Anna Michalik <anna.michalik@bluecrestinc.com>
2025-05-26 17:07:33 +0200

    dynamicly creating fields

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {CheckboxSettingsControl, EditBoxSettingsControl, SettingsPageControl, SettingsPageControlType} from "./models";
-import {FormArray, FormBuilder, ReactiveFormsModule} from "@angular/forms";
+import {FormArray, FormBuilder, FormGroup, ReactiveFormsModule} from "@angular/forms";
 import {EditBoxSettingsControlComponent} from "./edit-box-settings-control.component";
 import {CheckboxSettingsControlComponent} from "./checkbox-settings-control.component";
+import {MatStepper, MatStepperModule} from "@angular/material/stepper";
+import {CommonModule} from "@angular/common";
 
 @Component({
     selector: 'app-root',
@@ -11,19 +13,18 @@ import {CheckboxSettingsControlComponent} from "./checkbox-settings-control.comp
     imports: [
         EditBoxSettingsControlComponent,
         ReactiveFormsModule,
-        CheckboxSettingsControlComponent
+        MatStepperModule,
+        CheckboxSettingsControlComponent,
+        CommonModule
     ],
     styles: []
 })
 export class AppComponent implements OnInit {
-    formGroup = this._formBuilder.group({
-        formArray: this._formBuilder.array([
-            this._formBuilder.array([])
-        ])
-    });
-    formArray?: FormArray;
+    @ViewChild('stepper') stepper: MatStepper;
+    formGroup: FormGroup;
+    formArray: FormArray;
     testControl = this._formBuilder.control('test', []);
-    checkBoxControl = this._formBuilder.control('true', []);
+    checkBoxControl = this._formBuilder.control(true);
     settingControlsArray: SettingsPageControl[][] = [];
 
     constructor(private _formBuilder: FormBuilder) {
@@ -37,7 +38,16 @@ export class AppComponent implements OnInit {
         return this.settingControlsArray[step][ctrlNum];
     }
 
+    getControlType(step: number, ctrlNum: number): SettingsPageControlType {
+        return this.settingControlsArray[step][ctrlNum].Type;
+    }
+
     private initialData() {
+        this.formGroup = this._formBuilder.group({
+            formArray: this._formBuilder.array([
+                this._formBuilder.array([this.testControl, this.checkBoxControl])
+            ])
+        });
         this.formArray = this.formGroup.get('formArray') as FormArray;
         const ctrl: EditBoxSettingsControl = {
             Type: SettingsPageControlType.EditBox,
@@ -52,11 +62,16 @@ export class AppComponent implements OnInit {
             Value: 'false'
         }
         const firstStep = 0;
-        (this.formArray.controls[firstStep] as FormArray).push(this.testControl);
+        /*   let ary: FormArray = this._formBuilder.array([]);
+           ary.push(this.testControl);
+           ary.push(this.checkBoxControl);
+           (this.formArray.controls[firstStep] as FormArray).push(ary);*/
+
         this.settingControlsArray[firstStep] = [];
         this.settingControlsArray[firstStep][0] = ctrl;
         this.settingControlsArray[firstStep][1] = chBox;
     }
 
 
+    protected readonly SettingsPageControlType = SettingsPageControlType;
 }
